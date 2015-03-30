b0e9e158adf880a88643fd6604adc75ac831308e
Martin Disch <martindisch@gmail.com>
2015-03-30 20:52:53 +0200

    Removed debug option

--- app/src/main/java/com/martin/knowledgebase/LoginActivity.java
+++ app/src/main/java/com/martin/knowledgebase/LoginActivity.java
@@ -40,34 +40,6 @@ public class LoginActivity extends Activity {
         }
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            // TODO: Remove debug code
-            SharedPreferences prefs = getSharedPreferences("KB", MODE_PRIVATE);
-            SharedPreferences.Editor editor = prefs.edit();
-            editor.clear();
-            editor.commit();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public static class FragmentSetPassword extends Fragment {
 
         private EditText mFirst, mSecond;
