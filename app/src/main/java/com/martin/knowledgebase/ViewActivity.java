95ce3f625abb8bb7793d2c3788984092c8ddadd7
Martin Disch <martindisch@gmail.com>
2015-03-31 19:14:27 +0200

    Documented with results from testing

--- app/src/main/java/com/martin/knowledgebase/ViewActivity.java
+++ app/src/main/java/com/martin/knowledgebase/ViewActivity.java
@@ -25,17 +25,15 @@ public class ViewActivity extends Activity {
     }
 
     private void displayData() {
-        // TODO: Check if uid valid and/or singleton still in memory
+        /* At this point, the current instance of PlainStorage may have been lost when the activity has been in the background in a
+        low memory environment for an extended period of time. This could actually happen in all the places getting a new instance
+        of PlainStorage. I could enforce writing all the content to disk when briefly exiting the app to prevent data loss. However,
+        I believe the user (which is currently and will most likely remain me) should be free to leave the app running without all
+        this taking place, as the user is in fact aware of how the app works and will consider the consequences. */
         Entry entry = Util.getWithUid(PlainStorage.getInstance().getmEntries(), uid);
         mTitle.setText(entry.getTitle());
-        //mDate.setText(entry.getDate());
+        mDate.setText(entry.getDate());
         mText.setText(entry.getText());
-
-        // Log uid in case of memory loss - if uid is valid, the problem is the new instance missing any items,
-        // because they're only loaded in MainActivity (Critical design flaw here, PlainStorage.getInstance()
-        // should be loading entries.
-        // If uid is invalid (lower than zero or null), it's simply the uid having slipped out of memory.
-        mDate.setText(uid + "");
     }
 
     @Override
