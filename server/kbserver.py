005288d3112a5ba43e3fd4b309fb8cd22db13221
Martin Disch <martindisch@gmail.com>
2015-04-16 16:50:48 +0200

    Commented unicode airlock

--- server/kbserver.py
+++ server/kbserver.py
@@ -9,21 +9,21 @@ class MyTCPServer(SocketServer.ThreadingTCPServer):
 class MyTCPServerHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         try:
-            # data is received in utf-8 and decoded to byte strings
+            # data is received in utf-8 and decoded to unicode
             data = json.loads(self.request.recv(1024).strip().decode('utf-8'))
             if data['command'] == "ping":
                 print "Received ping"
-                # byte string data is converted to utf-8 in dumps()
+                # unicode data is converted to utf-8 in dumps()
                 self.request.sendall(json.dumps({'response': 'pong'}))
                 print "Sent pong"
             elif data['command'] == "entries":
                 print "Received entries"
                 files = [f for f in os.listdir("store") if os.path.isfile(os.path.join("store", f))]
-                # byte string data is converted to utf-8 in dumps()
                 self.request.sendall(json.dumps({'entries': list(reversed(files))}))
                 print "Sent entries"
             elif data['command'] == "get":
-                print "Received get "
+                print "Received get"
+                # read and decode utf-8 into unicode
                 backup = codecs.open("store/" + data['date'], 'r', encoding='utf-8')
                 content = backup.read()
                 backup.close()
@@ -32,12 +32,12 @@ class MyTCPServerHandler(SocketServer.BaseRequestHandler):
                 print "Sent content"
             elif data['command'] == "store":
                 print "Received store"
-                # stores byte string data in utf-8
+                # stores unicode data in utf-8
                 backup = codecs.open("store/" + data['date'], 'w', encoding='utf-8')
                 backup.write(data['data'])
                 backup.close()
                 print "File saved"
-                # byte string data is converted to utf-8 in dumps()
+                # unicode data is converted to utf-8 in dumps()
                 self.request.sendall(json.dumps({'response': 'ok'}))
                 print "Sent ok"
             else:
--- server/testclient.py
+++ server/testclient.py
@@ -3,11 +3,11 @@ import json
 
 while 1:
     data = input("What do you want to send? ")
-    json_string = json.dumps(data) # string data is converted to utf-8 in dumps()
+    json_string = json.dumps(data) # unicode data is converted to utf-8 in dumps()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(('127.0.0.1', 13373))
     s.send(json_string)
-    # data is sent in utf-8 and converted to unicode
+    # data is received in utf-8 and converted to unicode
     result = json.loads(s.recv(1024).decode('utf-8'))
     print json.dumps(result, indent=4, ensure_ascii=False)
     s.close()
