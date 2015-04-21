ce8cd2a29981136ae2461a2b347f48fe6ca884d4
Martin Disch <martindisch@gmail.com>
2015-04-21 08:53:57 +0200

    Some layout progress

--- app/src/main/java/com/martin/knowledgebase/BackupActivity.java
+++ app/src/main/java/com/martin/knowledgebase/BackupActivity.java
@@ -2,16 +2,30 @@ package com.martin.knowledgebase;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class BackupActivity extends Activity {
+
+    private RecyclerView mBackupList;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private Button mBackup, mRestore;
+    private TextView mCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_backup);
+
+        mBackupList = (RecyclerView) findViewById(R.id.rvBackups);
+        mBackupList.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mBackupList.setLayoutManager(mLayoutManager);
+        mCheck = (TextView) findViewById(R.id.tvStatus);
+        mBackup = (Button) findViewById(R.id.bBackup);
+        mRestore = (Button) findViewById(R.id.bRestore);
     }
 
 }
